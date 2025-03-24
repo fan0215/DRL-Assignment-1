83112f9124f2a54ae385e8b01d862a064d9eb0ac
fan0215 <zhifan215@gmail.com>
2025-03-24 23:45:42 +0800

    First Commit

--- student_agent.py
+++ student_agent.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 import random
 import gym
-with open("policy_table_pretrained.plk", "rb") as f:
+with open("q_table.plk", "rb") as f:
     Q_table = pickle.load(f)
 def get_action(obs):
     
@@ -13,17 +13,8 @@ def get_action(obs):
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
 
-
-    state_key = tuple(obs)  # 視你的 obs 結構而定，這裡假設 obs 是可轉換為 tuple 的
     
-    # 如果 Q_table 中有該狀態，選擇具有最高 Q 值的動作
-    if state_key in Q_table:
-        action = int(np.argmax(Q_table[state_key]))
-    else:
-        # 如果找不到狀態，回退到隨機策略
-        action = random.choice([0, 1, 2, 3, 4, 5])
-    
-    return action
+    return np.argmax(Q_table[(obs[0], obs[1], obs[10], obs[11], obs[12], obs[13])])
 
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
